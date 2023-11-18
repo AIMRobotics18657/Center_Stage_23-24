70a17565059a5c81c8a7e8dfcd1f5e93e90692d1
Dahlia  Zemmel <dahliazemmel@Dahlias-MacBook-Pro.local>
2023-11-17 21:31:02 -0500

    Please check this over and see if it works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/PAL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/PAL.java
@@ -2,17 +2,30 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.util.Mechanism;
 
 public class PAL extends Mechanism {
+    Servo PAL;
+
+    public String PaperAirplane = "PAL";
+
     @Override
     public void init(HardwareMap hwMap) {
-
+        PAL = hwMap.get(Servo.class, PaperAirplane);
     }
 
     @Override
-    public void loop(Gamepad gamepad, Gamepad gamepad2) {
-
+    public void loop(Gamepad gamepad) {
+        if (gamepad.dpad_up) {
+            PAL.setPosition(1);
+        } else if (gamepad.dpad_down) {
+            PAL.setPosition(-1);
+        } else {
+            PAL.setPosition(0);
+        }
     }
 }
+
+
